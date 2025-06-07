62ed77067e623411855b351110a93ea75eae0444
HoangNV <90242291+hoangnv812001@users.noreply.github.com>
2025-06-07 15:02:03 +0700

    Update BlogController.java
    update CRUD

--- src/main/java/com/swp391/hivtmss/controller/BlogController.java
+++ src/main/java/com/swp391/hivtmss/controller/BlogController.java
@@ -4,6 +4,7 @@ import com.swp391.hivtmss.model.payload.request.BlogRequest;
 import com.swp391.hivtmss.model.payload.response.BlogResponse;
 import com.swp391.hivtmss.service.BlogService;
 import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/blog")
+@RequestMapping("/api/blogs")
 public class BlogController {
     @Autowired
     private BlogService blogService;
@@ -24,14 +25,14 @@ public class BlogController {
         return new ResponseEntity<>(createBlog, HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<BlogResponse> getBlogById(@PathVariable Long id) {
+    @GetMapping
+    public ResponseEntity<BlogResponse> getBlogById(@PathParam("id") Long id) {
         BlogResponse blog = blogService.getBlogById(id);
         return ResponseEntity.ok(blog);
     }
 
-    @GetMapping("/account/{accountId}")
-    public ResponseEntity<BlogResponse> getBlogByAccountId(@PathVariable UUID accountId) {
+    @GetMapping("/account")
+    public ResponseEntity<BlogResponse> getBlogByAccountId(@PathParam("accountId") UUID accountId) {
         BlogResponse blog = blogService.getBlogByAccountId(accountId);
         return ResponseEntity.ok(blog);
     }
@@ -42,15 +43,15 @@ public class BlogController {
         return ResponseEntity.ok(blog);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<BlogResponse> updateBlog(@PathVariable Long id,
+    @PutMapping
+    public ResponseEntity<BlogResponse> updateBlog(@PathParam("/{id}") Long id,
                                                                    @Valid @RequestBody BlogRequest blogRequest) {
         BlogResponse updatedBlog = blogService.updateBlog(id, blogRequest);
         return ResponseEntity.ok(updatedBlog);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteBlog(@PathVariable Long id) {
+    @DeleteMapping
+    public ResponseEntity<Void> deleteBlog(@PathParam("id") Long id) {
         blogService.deleteBlog(id);
         return ResponseEntity.noContent().build();
     }
