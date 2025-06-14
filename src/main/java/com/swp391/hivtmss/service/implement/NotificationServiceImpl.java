da81f6e38e935782996d558b9f8dc3bf3e10c518
Tinhhhh <tinhvvse173112@fpt.edu.vn>
2025-06-14 18:25:45 +0700

    fix: implement Noti

--- src/main/java/com/swp391/hivtmss/service/implement/NotificationServiceImpl.java
+++ src/main/java/com/swp391/hivtmss/service/implement/NotificationServiceImpl.java
@@ -5,7 +5,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
-public class NotificationServiceImpl extends NotificationService {
+public class NotificationServiceImpl implements NotificationService {
+
+
+    @Override
+    public void newAppointment(UUID doctorId, UUID customerId) {
+
+    }
+
+    @Override
+    public void appointmentFinished(UUID customerId) {
+
+    }
+
+    @Override
+    public void appointmentCancelled(UUID doctorId, String reason) {
+
+    }
+
+    @Override
+    public void appointmentDoctorChange(UUID oldDoctorId, UUID customerId, UUID newDoctorId) {
+
+    }
 }
