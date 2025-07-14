6b86d6391e5e76d21d86cc8209d85d2a68fdbaf6
HoangNV <90242291+hoangnv812001@users.noreply.github.com>
2025-07-14 12:17:54 +0700

    BLog
    Fix update ko còn bắt add file ảnh mới

--- src/main/java/com/swp391/hivtmss/model/payload/request/BlogRequest.java
+++ src/main/java/com/swp391/hivtmss/model/payload/request/BlogRequest.java
@@ -2,6 +2,7 @@ package com.swp391.hivtmss.model.payload.request;
 
 import com.swp391.hivtmss.model.entity.BlogImg;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -22,6 +23,7 @@ public class BlogRequest {
     private String title;
 
     @Schema(description = "Nội dung bài viết", required = true)
+    @Column(columnDefinition = "TEXT")
     private String content;
 
     @Schema(description = "ID tài khoản người tạo blog", required = true)
